8efaecd065115442b63373a90e4d77b498ff6c98
NedNoodleHead <spencer.braak@gmail.com>
2024-01-08 16:25:09 -0800

    gitignore and db :D

--- db/create_db.py
+++ db/create_db.py
@@ -1,25 +1,23 @@
 import sqlite3
 
 def create_db():
-     conn = sqlite3.connect("main.db") # creates if not created already
-     conn.execute("""
-               CREATE TABLE IF NOT EXISTS main (
-                    name text,
-                    serial text,
-                    manufacturer text,
-                    price decimal (6, 2),
-                    assetcategory text,  
-                    assettype text,
-                    assignedto text,
-                    assetlocation text,
-                    purchasedate datetime,
-                    installdate datetime,
-                    replacementdate datetime,
-                    notes mediumtext,
-                    status bool,                    
-                    uniqueid text PRIMARY KEY
-                  )
-
-
-                       
-                  """)
+      with sqlite3.connect("main.db") as conn: # creates if not created already
+          conn.execute("""
+                    CREATE TABLE IF NOT EXISTS main (
+                         name text,
+                         serial text,
+                         manufacturer text,
+                         price decimal (6, 2),
+                         assetcategory text,  
+                         assettype text,
+                         assignedto text,
+                         assetlocation text,
+                         purchasedate date,
+                         installdate date,
+                         replacementdate date,
+                         notes mediumtext,
+                         status bool,                    
+                         uniqueid text PRIMARY KEY
+                       )
+                       """)
+          conn.commit()
