4975ce8da7e516c77956a9990b576e3dae2a7887
NedNoodleHead <spencer.braak@gmail.com>
2024-01-10 15:22:37 -0800

    new data type (will probably clean up into inheritance later)

--- util/data_types.py
+++ util/data_types.py
@@ -46,4 +46,39 @@ def create_inventory_object(name: str, serial: str, manufacturer: str, price: fl
      id = str(uuid4())
      return InventoryObject(name, serial, manufacturer, price, assetcategory, assettype, assignedto, assetlocation, purchasedate, installdate, replacementdate, notes, True, id)
      
+class TableObject:
+     # same as inventory object, but no self.enabled
+     def __init__(self, name: str, serial: str, manufacturer: str, price: float, assetcategory: str, assettype: str, assignedto: str, assetlocation: str, purchasedate: datetime.date, installdate: datetime.date, replacementdate: datetime.date, notes: str, uniqueid: str):
+          self.name = name  # name of device. e.g. SPENCER-APC
+          self.serial = serial  # serial number of device
+          self.manufacturer = manufacturer  # manufacturer of the device
+          self.price = price  # cost of device at time of purchase
+          self.assetcategory = assetcategory  # general category of object (network, security, software, printing)
+          self.assettype = assettype  # what type of asset is it? (router, switch, all-in-one)
+          self.assignedto = assignedto  # what person / dept is asset assigned to?
+          self.assetlocation = assetlocation  # where is the asset? e.g. GOVH, HLTH (enum to pick from)
+          self.purchasedate = purchasedate  # day item was purchased
+          self.installdate = installdate  # day item was installed,
+          self.replacementdate = replacementdate  # day the item should be replaced (years ahead from installdate. hardware = 4yr, print = 5yr)
+          self.notes = notes  # Will either be something (which will become a button to view notes), or nothing, that will be "No Notes"
+          self.uniqueid = uniqueid  # uuid-4 for the device
+          # similar to 
+     def __iter__(self):
+         yield self.name 
+         yield self.serial   
+         yield self.manufacturer  
+         yield self.price 
+         yield self.assetcategory
+         yield self.assettype
+         yield self.assignedto 
+         yield self.assetlocation
+         yield self.purchasedate 
+         yield self.installdate  
+         yield self.replacementdate 
+         yield self.notes
+         yield self.uniqueid           
+     def __str__(self):
+          return f"Inventory_Object: {self.name} id: {self.uniqueid}"          
+     def __repr__(self):
+          return f"Inventory_Object: {self.name} id: {self.uniqueid}"          
                     
