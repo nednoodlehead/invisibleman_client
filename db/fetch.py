af86f1a10a7911c70f354d12ce3bcc0bddccfcec
NedNoodleHead <spencer.braak@gmail.com>
2024-01-10 15:19:22 -0800

    new function for new data type (get all)

--- db/fetch.py
+++ db/fetch.py
@@ -1,5 +1,5 @@
 import sqlite3
-from util.data_types import InventoryObject
+from util.data_types import InventoryObject, TableObject
 
 
 def fetch_all() -> [InventoryObject]:
@@ -10,3 +10,10 @@ def fetch_all() -> [InventoryObject]:
                return_list.append(InventoryObject(*item))
      return return_list
                          
+def fetch_all_for_table() -> [TableObject]:
+     return_list = []
+     with sqlite3.connect("main.db") as conn:
+          data = conn.execute("SELECT name, serial, manufacturer, price, assetcategory, assettype, assignedto, assetlocation, purchasedate, installdate, replacementdate, notes, uniqueid FROM main WHERE status = 1")
+          for item in data:
+               return_list.append(TableObject(*item))
+     return return_list
