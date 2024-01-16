6466eae75e0ee95c27bcdba334137acf1da3cd49
NedNoodleHead <spencer.braak@gmail.com>
2024-01-15 16:25:18 -0800

    changed volatile inserting, and resulting json structure

--- volatile/write_to_volatile.py
+++ volatile/write_to_volatile.py
@@ -5,9 +5,18 @@
 import json
 
 def add_to_asset_list(name: str, years: int):
-     with open("./volatile\\assetcategory.json", "r") as f:
+     with open("./volatile/assetcategory.json", "r") as f:
           raw = json.load(f)
-          raw[name] = years
+          raw["Category"][name] = years
           json_data = json.dumps(raw, indent=4)
-     with open(".\\volatile\\assetcategory.json", "w") as w:  # stupid, didnt work otherwise..
+     with open("./volatile/assetcategory.json", "w") as w:  # stupid, didnt work otherwise..
           w.write(json_data)
+
+def add_to_type_or_location(new: str, type_or_loc: str):
+     with open("./volatile/assetcategory.json", "r") as f:
+          raw = json.load(f)
+          raw[type_or_loc].append(new)
+          json_data = json.dumps(raw, indent=4)
+     with open("./volatile/assetcategory.json", "w") as w:  # stupid, didnt work otherwise..
+          w.write(json_data)
+
