5a886a024b3c634b46ee867c3ece3bd410f18cac
NedNoodleHead <spencer.braak@gmail.com>
2024-01-18 10:43:59 -0800

    needed to change the gui so it can handle more items (mostly for the locations json)

--- gui/add_item_window.py
+++ gui/add_item_window.py
@@ -12,80 +12,83 @@ class GenericAddJsonWindow(QWidget):
           # value should be either: "Category", "Type", "Location"
           # should each of these have their own file? or one file?
 
-
-
           font = QFont()
           font.setPointSize(10)
           self.setFont(font)
-          self.json_write_to_button = QPushButton("Update", self)
+          self.json_write_to_button = QPushButton(self)
           self.json_write_to_button.setObjectName(u"json_write_to_button")
-          self.json_write_to_button.setGeometry(QRect(10, 410, 75, 23))
-          self.json_exit_button = QPushButton("Exit", self)
+          self.json_write_to_button.setGeometry(QRect(370, 380, 75, 23))
+          self.json_exit_button = QPushButton(self)
           self.json_exit_button.setObjectName(u"json_exit_button")
           self.json_exit_button.setGeometry(QRect(370, 410, 75, 23))
           self.json_new_value_text = QLineEdit(self)
           self.json_new_value_text.setObjectName(u"json_new_value_text")
-          self.json_new_value_text.setGeometry(QRect(130, 270, 113, 20))
-          self.json_new_value_label = QLabel("Add new value", self)
+          self.json_new_value_text.setGeometry(QRect(320, 30, 113, 20))
+          self.json_new_value_label = QLabel(self)
           self.json_new_value_label.setObjectName(u"json_new_value_label")
-          self.json_new_value_label.setGeometry(QRect(80, 270, 51, 16))
+          self.json_new_value_label.setGeometry(QRect(360, 10, 51, 16))
           self.json_conditional_years_text = QLineEdit(self)
           self.json_conditional_years_text.setObjectName(u"json_conditional_years_text")
-          self.json_conditional_years_text.setGeometry(QRect(130, 310, 113, 20))
-          self.json_conditional_years_label = QLabel("Years until expiry", self)
+          self.json_conditional_years_text.setGeometry(QRect(320, 120, 113, 20))
+          self.json_conditional_years_label = QLabel(self)
           self.json_conditional_years_label.setObjectName(u"json_conditional_years_label")
-          self.json_conditional_years_label.setGeometry(QRect(20, 310, 111, 21))
+          self.json_conditional_years_label.setGeometry(QRect(330, 90, 111, 21))
           self.json_value_frame = QFrame(self)
           self.json_value_frame.setObjectName(u"json_value_frame")
-          self.json_value_frame.setGeometry(QRect(90, 40, 201, 211))
+          self.json_value_frame.setGeometry(QRect(50, 0, 201, 441))
           self.json_value_frame.setFrameShape(QFrame.StyledPanel)
           self.json_value_frame.setFrameShadow(QFrame.Raised)
           self.verticalLayoutWidget = QWidget(self.json_value_frame)
           self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
-          self.verticalLayoutWidget.setGeometry(QRect(-1, 0, 211, 211))
+          self.verticalLayoutWidget.setGeometry(QRect(-1, 0, 211, 441))
           self.json_value_layout = QVBoxLayout(self.verticalLayoutWidget)
           self.json_value_layout.setObjectName(u"json_value_layout")
           self.json_value_layout.setContentsMargins(0, 0, 0, 0)
           self.json_years_frame = QFrame(self)
           self.json_years_frame.setObjectName(u"json_years_frame")
-          self.json_years_frame.setGeometry(QRect(290, 40, 41, 211))
+          self.json_years_frame.setGeometry(QRect(250, 0, 41, 441))
+          self.json_years_frame.setStyleSheet(u"")
           self.json_years_frame.setFrameShape(QFrame.StyledPanel)
           self.json_years_frame.setFrameShadow(QFrame.Raised)
           self.verticalLayoutWidget_2 = QWidget(self.json_years_frame)
           self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
-          self.verticalLayoutWidget_2.setGeometry(QRect(0, 0, 41, 211))
+          self.verticalLayoutWidget_2.setGeometry(QRect(0, 0, 41, 441))
           self.json_years_layout = QVBoxLayout(self.verticalLayoutWidget_2)
           self.json_years_layout.setObjectName(u"json_years_layout")
           self.json_years_layout.setContentsMargins(0, 0, 0, 0)
           self.json_button_frame = QFrame(self)
           self.json_button_frame.setObjectName(u"json_button_frame")
-          self.json_button_frame.setGeometry(QRect(50, 40, 41, 211))
+          self.json_button_frame.setGeometry(QRect(10, 0, 41, 441))
           self.json_button_frame.setFrameShape(QFrame.StyledPanel)
           self.json_button_frame.setFrameShadow(QFrame.Raised)
           self.verticalLayoutWidget_3 = QWidget(self.json_button_frame)
           self.verticalLayoutWidget_3.setObjectName(u"verticalLayoutWidget_3")
-          self.verticalLayoutWidget_3.setGeometry(QRect(0, 0, 41, 211))
+          self.verticalLayoutWidget_3.setGeometry(QRect(0, 0, 41, 441))
           self.json_button_layout = QVBoxLayout(self.verticalLayoutWidget_3)
           self.json_button_layout.setObjectName(u"json_button_layout")
           self.json_button_layout.setContentsMargins(0, 0, 0, 0)
-          self.json_values_label = QLabel("Values", self)
-          self.json_values_label.setObjectName(u"json_values_label")
-          self.json_values_label.setGeometry(QRect(160, 20, 51, 16))
-          self.json_years_label = QLabel("Years", self)  # also conditional...
-          self.json_years_label.setObjectName(u"json_years_label")
-          self.json_years_label.setGeometry(QRect(290, 20, 61, 16))
-          self.json_add_new_value_button = QPushButton("Add new value", self)
+          self.json_add_new_value_button = QPushButton(self)
           self.json_add_new_value_button.setObjectName(u"json_add_new_value_button")
-          self.json_add_new_value_button.setGeometry(QRect(250, 270, 91, 23))
+          self.json_add_new_value_button.setGeometry(QRect(330, 170, 91, 23))
           font1 = QFont()
           font1.setPointSize(8)
           self.json_add_new_value_button.setFont(font1)
-          self.json_add_new_value_button.setFont(font1)        
+          self.json_write_to_button.setText(u"Update")
+          self.json_exit_button.setText(u"Exit")
+          self.json_new_value_label.setText(u"Value")
+          self.json_conditional_years_text.setText("")
+          self.json_conditional_years_label.setText(u"Years until expiry")
+          self.json_add_new_value_button.setText(u"Add new value")
+          font1 = QFont()
+          font1.setPointSize(8)
+          self.json_add_new_value_button.setFont(font1)
+
+
+
           if self.target != "Category":
                 self.json_conditional_years_label.hide()
                 self.json_conditional_years_text.hide()
                 self.json_years_frame.setStyleSheet("")
-                self.json_years_label.hide()
           self.fill_layout_with_content()
           self.json_value_frame.raise_()
           self.json_add_new_value_button.clicked.connect(self.add_new_value_from_user)
@@ -124,12 +127,12 @@ class GenericAddJsonWindow(QWidget):
                     button = self.make_x_button()
                     self.json_button_layout.addWidget(button)
                     
-     def make_x_button(self):
+     def make_x_button(self): # func to make the x and delete button for the little psuedo-menu
           button = QPushButton("X")
           button.clicked.connect(lambda: self.button_remove_item(button))
           return button
      
-     def button_remove_item(self,button):
+     def button_remove_item(self,button):  # func to bind to the button, to remove row
           index = self.json_button_layout.indexOf(button)
           self.remove_wid(index)
 
@@ -151,24 +154,23 @@ class GenericAddJsonWindow(QWidget):
                button = self.make_x_button()
                self.json_button_layout.addWidget(button)
           else:
-               # maybe make prompt to tell user to be smart
-               print("please enter value")
-          
+               # maybe make prompt to tell user to be smart (srry alex lol)
+               pass  # maybe do something with one of those little status bars      
      def write_changes(self):
-          data = []
-          length = range(self.json_value_layout.count())
+          length = range(self.json_value_layout.count())  # get how many times we have to iter over layout to get all items
           if self.target == "Category":
                data = {}
                for index in length:
                     key = self.json_value_layout.itemAt(index).widget().text()
                     val = self.json_years_layout.itemAt(index).widget().text()
-                    data[key] = val
+                    data[key] = val  # create dict key, val pair to put into json
           else:
+               data = []
                for index in length:
-                    val = self.json_value_layout.itemAt(index).widget().text()
+                    val = self.json_value_layout.itemAt(index).widget().text()  # get the data from the combobox
                     data.append(val)
-          replace_json_target(self.target, data)
-          self.parent_window.refresh_combobox(self.target)
+          replace_json_target(self.target, data)  # replace the json with the new data
+          self.parent_window.refresh_combobox(self.target)  # send signal to main window to refresh comboboxes
           self.close()                   
           # write changes to json file
           # probably just pull from the layouts and create a dict, then write it?
