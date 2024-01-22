2813f0e18a3ea85347708e1cf31ec273338a126b
NedNoodleHead <spencer.braak@gmail.com>
2024-01-22 13:49:20 -0800

    updating a single entry :)

--- db/update.py
+++ db/update.py
@@ -1,8 +1,28 @@
 import sqlite3
+from util.data_types import InventoryObject
 
 def update_notes(notes: str, uuid: str):
      with sqlite3.connect("main.db") as conn:
           conn.execute("UPDATE main SET notes = ? WHERE uniqueid = ?", (notes, uuid))
           conn.commit()
-          
 
+def update_full_obj(obj: InventoryObject):
+     with sqlite3.connect("main.db") as conn:
+          conn.execute("""
+                       UPDATE main 
+                       SET name = ?,
+                       SET serial = ?,
+                       SET manufacturer = ?,
+                       SET price = ?,
+                       SET assetcategory = ?,
+                       SET assettype = ?,
+                       SET assignedto = ?,
+                       SET assetlocation = ?,
+                       SET purchasedate = ?,
+                       SET installdate = ?,
+                       SET replacementdate = ?,
+                       SET notes = ?,
+                       SET status = ?,
+                       WHERE uniqueid = ?
+                        """,
+                       *obj)
