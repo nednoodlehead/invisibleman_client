d3b90ebe467ed989b38b6659c75ceea804350a5d
NedNoodleHead <spencer.braak@gmail.com>
2024-01-26 14:43:25 -0800

    default main file

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # inventory / asset management application
-from db import create_db
-from db import insert
+from db import create_db, insert, fetch
 from util.data_types import create_inventory_object
 import datetime
+from gui.start import main
 
-m = create_inventory_object("namer", "serial :DD", "asus?", 10.00, "HARDWARE", "computer",
-                            "nedly", "gov", datetime.date.today(), datetime.date.today(), "no notes here..")
-insert.new_entry(m)
+main()
