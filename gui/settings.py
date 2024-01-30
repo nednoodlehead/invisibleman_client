061cc0fd2e748f41c35095da51ffee3d2246de06
NedNoodleHead <spencer.braak@gmail.com>
2024-01-30 09:57:30 -0800

    setting for changing to and from dark mode :)

--- gui/settings.py
+++ gui/settings.py
@@ -1,3 +1,20 @@
-# saves settings using QSettings()
-from PyQt5.QtCore import QSettings
+# ok, so maybe there is an actual argument for using PyQt5.QtCore.QSettings, but it seems like
+# honestly more work than just a json that i dump into and read on start
+# plus it is probably easier to externally configure if something goes wrong with the appplication
+from PyQt5.QtWidgets import QWidget, QStyleFactory
+from PyQt5.QtGui import QPalette, QColor, QGuiApplication
+from PyQt5.QtCore import Qt, QFile, QTextStream
+# flip for dark / light mode
 
+
+
+def dark_light_mode_switch(self: QWidget, is_dark: bool):  # cant do MainProgram cause of circular import :(
+     if is_dark:
+          file = QFile("./gui/styles/dark.qss")
+          file.open(QFile.ReadOnly | QFile.Text)
+          stream = QTextStream(file)
+          self.setStyleSheet(stream.readAll())
+     else:
+          self.setStyleSheet("")  # lol bye bye style sheet 
+          # resets it to default bte
+     
