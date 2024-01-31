52662673391da5ec68a2135061571b9668b082b1
NedNoodleHead <spencer.braak@gmail.com>
2024-01-31 15:21:23 -0800

    run this file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# inventory / asset management application
-from db import create_db, insert, fetch
-from util.data_types import create_inventory_object
-import datetime
 from gui.start import main
+
+# asset management tracker!
+# built with PyQt5 (<3 Qtdesigner) & Sqlite
+# this is where the program begins
+
 
 main()
