b4d9347f27a1812aa95855d62a7d63aceee72208
NedNoodleHead <spencer.braak@gmail.com>
2024-02-15 14:12:29 -0800

    undid the 'fix' in question. seems impossible to change the stupid scaling property.

--- gui/start.py
+++ gui/start.py
@@ -1,13 +1,11 @@
 from gui.ui_functions import MainProgram
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
-from PyQt5.QtCore import Qt
 
 # thank you: https://stackoverflow.com/questions/40817687/adding-functions-to-buttons-via-importing-pyqt5-ui
 # idk why pyqt5 is so hard comparatively
 
 def main():
-     QApplication.setAttribute(Qt.AA_DisableHighDpiScaling)
      app = QApplication(sys.argv)
      ui = MainProgram()
      ui.show()
