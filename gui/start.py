3b2a6d4c8ab0722ed5a17f5a6d054cb326653f5f
NedNoodleHead <spencer.braak@gmail.com>
2024-02-15 12:00:22 -0800

    fixed so windows doesn't scale the text weirdly

--- gui/start.py
+++ gui/start.py
@@ -1,6 +1,10 @@
 from gui.ui_functions import MainProgram
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+import ctypes
+
+# change the dpi awareness to per-monitor aware. Makes it so text scaling is not an issue when window's display scale > 100
+ctypes.windll.shcore.SetProcessDpiAwareness(2)
 
 # thank you: https://stackoverflow.com/questions/40817687/adding-functions-to-buttons-via-importing-pyqt5-ui
 # idk why pyqt5 is so hard comparatively
