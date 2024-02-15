cf809a37bd6365154ba9957babfd53c7f692c80e
NedNoodleHead <spencer.braak@gmail.com>
2024-02-15 12:57:40 -0800

    this one should work

--- gui/start.py
+++ gui/start.py
@@ -1,15 +1,13 @@
 from gui.ui_functions import MainProgram
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
-import ctypes
-
-# change the dpi awareness to per-monitor aware. Makes it so text scaling is not an issue when window's display scale > 100
-ctypes.windll.shcore.SetProcessDpiAwareness(2)
+from PyQt5.QtCore import Qt
 
 # thank you: https://stackoverflow.com/questions/40817687/adding-functions-to-buttons-via-importing-pyqt5-ui
 # idk why pyqt5 is so hard comparatively
 
 def main():
+     QApplication.setAttribute(Qt.AA_DisableHighDpiScaling)
      app = QApplication(sys.argv)
      ui = MainProgram()
      ui.show()
