56c0598295f7de8d5394a723fdc3dc6d4d9d3968
NedNoodleHead <spencer.braak@gmail.com>
2025-12-09 08:31:38 -0800

    swapped back to the old page order

--- gui/auto.py
+++ gui/auto.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'interfaceKPtsUy.ui'
+## Form generated from reading UI file 'interfaceSPNsef.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.14.1
 ##
@@ -692,7 +692,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.stackedWidget.setCurrentIndex(5)
+        self.stackedWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
