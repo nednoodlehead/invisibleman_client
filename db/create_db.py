fcf08c491b383f65ee9c6cd66bb82d2420e95ed1
NedNoodleHead <spencer.braak@gmail.com>
2026-02-06 08:35:58 -0800

    new return and loan date, also will be updating the bottom section with some triggers and whatnot soon

--- db/create_db.py
+++ db/create_db.py
@@ -1,34 +1,34 @@
-import sqlite3
-
+import psycopg2
 
 # this is not used anymore, we will leave this here though, since it shows our table
-def create_db():
+def create_db(conn):
     # honestly there is probably some merit in messing with the nullif keywrods to store null better (and handle it better in invisman)
     # is_local is for computers that are not registered in intune. primarily adding so we can view the intune report more accurately
-    with sqlite3.connect("main.db") as conn:  # creates if not created already
-        conn.execute(
-            """
-             CREATE TABLE IF NOT EXISTS main (
-                         assettype text,
-                         manufacturer text,
-                         serial text,
-                         model text,
-                         cost decimal (6, 2),
-                         assignedto text,
-                         name text,
-                         assetlocation text,
-                         assetcategory text,
-                         deploymentdate date,
-                         replacementdate date,
-                         retirementdate date,
-                         notes mediumtext,
-                         status bool,
-                         is_local bool,
-                         uniqueid text PRIMARY KEY
-                       )
-                       """
-        )
-        conn.commit()
+    conn.execute(
+        """
+         CREATE TABLE IF NOT EXISTS main (
+                     assettype text,
+                     manufacturer text,
+                     serial text,
+                     model text,
+                     cost decimal (6, 2),
+                     assignedto text,
+                     name text,
+                     assetlocation text,
+                     assetcategory text,
+                     deploymentdate date,
+                     replacementdate date,
+                     retirementdate date,
+                     notes mediumtext,
+                     status bool,
+                     is_local bool,
+                     loandate date,
+                     returndate date
+                     uniqueid text PRIMARY KEY
+                   )
+                   """
+    )
+    conn.commit()
 
 
 """
@@ -47,3 +47,16 @@ create table changed
     edit_date date
 
 """
+
+"""
+Everything in this section are additional tables and triggers that need to be configured.
+This comprises of two primary functions. One that creates the tables (which will also run alongside create_db(), and one to create triggers)
+The tables can be ran normall, but the triggers will require the installation of plpython3u on the server
+Typically it would seem advantageous to automate such a task, but i sort of dislike the prospect of the function being untrusted in the sense of anyone connected can run anything.
+
+Yeah, I know anyone added to this program will be trusted, that is the entire point, but I still don't love the idea. Even from a "compromised credentials" view. (I'm not even sure if that is a valid complaint or not tbh)
+So I'd rather recommend just copy pasting the stuff in. it is also how it is being run initially, since I'm fixing things and adapating on the fly!
+"""
+# def create_auditing_trigger_and_function(conn):
+    # we don't need to audit the delete functions since there isn't any deleting operations in the database 
+    
